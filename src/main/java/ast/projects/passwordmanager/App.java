ce9a8eca848a6036009b3f3535affc73ad2b5196
marco <marco@computer.station>
2024-07-31 17:44:16 +0200

    First commit maven with pit jacoco and log4j

---
+++ src/main/java/ast/projects/passwordmanager/App.java
@@ -0,0 +1,12 @@
+package ast.projects.passwordmanager;
+
+import org.apache.logging.log4j.LogManager;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        LogManager.getLogger().info("This is an info message from src code");
+    }
+}
